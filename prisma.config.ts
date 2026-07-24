cbd06bd03e2ccf0d37e012b77c966ad54be33a76
unknown <jeanvictorbr@gmail.com>
2026-07-24 07:12:36 -0300

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
   schema: './prisma/schema.prisma',
-  migrate: {
-    databaseUrl: process.env.DATABASE_URL,
-  },
+  earlyAccess: true,
+  // A propriedade 'url' DEVE estar definida para o 'db push' funcionar no Prisma 7
+  url: process.env.DATABASE_URL, 
 });
