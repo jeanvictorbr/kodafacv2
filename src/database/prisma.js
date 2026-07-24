057ed2250904f057d71ae9f5982e6c3cfa9b2443
unknown <jeanvictorbr@gmail.com>
2026-07-24 05:21:01 -0300

    dsa

--- src/database/prisma.js
+++ src/database/prisma.js
@@ -1,17 +1,5 @@
 const { PrismaClient } = require('@prisma/client');
 
-// Cria o Client do Prisma
-const prismaClientSingleton = () => {
-  return new PrismaClient();
-};
-
-// Evita que o hot-reload do Node multiplique as conexões (Padrão Singleton)
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-
-if (process.env.NODE_ENV !== 'production') {
-    globalThis.prismaGlobal = prisma;
-}
-
-console.log('[BANCO DE DADOS] Prisma ORM conectado com sucesso na base.');
+const prisma = new PrismaClient();
 
 module.exports = prisma;
