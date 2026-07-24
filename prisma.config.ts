e669b6e8eddddd1391d4cef2cb852ca40efa8894
unknown <jeanvictorbr@gmail.com>
2026-07-24 13:59:16 -0300

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,9 @@
 import { defineConfig } from '@prisma/config';
 
 export default defineConfig({
-  schema: './prisma/schema.prisma',
-  earlyAccess: true,
-  // Força o Prisma a procurar no ambiente (Discloud) ou no arquivo local
-  url: process.env.DATABASE_URL, 
+  migrate: {
+    database: {
+      url: process.env.DATABASE_URL,
+    },
+  },
 });
