13375cc4f8979d694420b70b0dbb480473a5f494
unknown <jeanvictorbr@gmail.com>
2026-07-25 03:01:04 -0300

    dsa

--- src/database/prisma.js
+++ src/database/prisma.js
@@ -1,14 +1,5 @@
-const { PrismaClient } = require('./generated/client');
-const { Pool } = require('pg');
-const { PrismaPg } = require('@prisma/adapter-pg');
+const { PrismaClient } = require('@prisma/client');
 
-// 1. Cria a pool de conexão do PostgreSQL
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-
-// 2. Cria o adaptador pro Prisma
-const adapter = new PrismaPg(pool);
-
-// 3. Passa o adaptador no construtor do Prisma (Exigência do v7)
-const prisma = new PrismaClient({ adapter });
+const prisma = new PrismaClient();
 
 module.exports = prisma;
